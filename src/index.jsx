6954879d357229e056b3f0790b681ff5ee6c862d
Vitaliy <vitalikrud03@example.com>
2023-12-22 15:25:35 +0300

    test: tesgin husky

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+console.log('test');
+
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
