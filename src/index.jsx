774587a559db0108734a13362d72f2b640a5e821
Vitaliy <vitalikrud03@example.com>
2023-12-24 15:43:52 +0300

    refactor: add folders and neccesary files

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,5 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-console.log('test');
-
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
