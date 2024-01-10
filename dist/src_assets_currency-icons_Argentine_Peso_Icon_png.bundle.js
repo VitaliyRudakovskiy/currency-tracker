7e06ed9a3d09bbfd8fc5cf64ba8895f4610ab4d8
Vitaliy <vitalikrud03@example.com>
2024-01-11 00:15:49 +0300

    refactor: try to add vercel

---
+++ dist/src_assets_currency-icons_Argentine_Peso_Icon_png.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcurrency_tracker"] = self["webpackChunkcurrency_tracker"] || []).push([["src_assets_currency-icons_Argentine_Peso_Icon_png"],{
+
+/***/ "./src/assets/currency-icons/Argentine Peso Icon.png":
+/*!***********************************************************!*\
+  !*** ./src/assets/currency-icons/Argentine Peso Icon.png ***!
+  \***********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"72f4fd7c2f797e9d5ed107ea8dbd7342.png\");\n\n//# sourceURL=webpack://currency-tracker/./src/assets/currency-icons/Argentine_Peso_Icon.png?");
+
+/***/ })
+
+}]);
---
+++ dist/src_assets_currency-icons_Turkish_Lira_Icon_png.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcurrency_tracker"] = self["webpackChunkcurrency_tracker"] || []).push([["src_assets_currency-icons_Turkish_Lira_Icon_png"],{
+
+/***/ "./src/assets/currency-icons/Turkish Lira Icon.png":
+/*!*********************************************************!*\
+  !*** ./src/assets/currency-icons/Turkish Lira Icon.png ***!
+  \*********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"cd10bea1958ba506536c56da4c42ebf1.png\");\n\n//# sourceURL=webpack://currency-tracker/./src/assets/currency-icons/Turkish_Lira_Icon.png?");
+
+/***/ })
+
+}]);
